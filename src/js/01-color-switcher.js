b7d11f8add975a6f2ca395f91ccf215d3538b746
fhlare <127776539+fhlare@users.noreply.github.com>
2023-09-20 19:27:26 +0300

    end 3 tasks hw

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+  buttonStart: document.querySelector('[data-start]'),
+  buttonStop: document.querySelector('[data-stop]'),
+};
 
+refs.buttonStart.addEventListener('click', onSwitchColorClick);
+refs.buttonStop.addEventListener('click', onStopSwitchColorClick);
+
+let intervalId = null;
+
+function switchDisabled(boule) {
+  refs.buttonStart.disabled = boule;
+  refs.buttonStop.disabled = !boule;
+}
+
+function onSwitchColorClick() {
+  switchDisabled(true);
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopSwitchColorClick() {
+  switchDisabled(false);
+
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
